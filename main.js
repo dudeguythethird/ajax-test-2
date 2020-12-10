11a1ab67342f4b055daedc5eb699771922a8df0f
dudeguythethird <keablejoseph@gmail.com>
2020-12-10 11:52:19 +0000

    initial commit

---
+++ main.js
@@ -0,0 +1,22 @@
+const baseURL = "https://ci-swapi.herokuapp.com/api/"
+
+function getData(type, cb) {
+
+    var xhr = new XMLHttpRequest();
+
+    xhr.open("GET", baseURL + type + "/");
+    xhr.send();
+
+    xhr.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            cb(JSON.parse(this.responseText));
+        }
+    };
+}
+
+function writeToDocument(type) {
+    getData(type, function(){
+        console.dir(data);
+        document.getElementById("data").innerHTML = data;
+    })
+}
